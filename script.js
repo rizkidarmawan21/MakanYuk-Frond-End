cb98f49c3002d24f1c07dfad85a6631534be867a
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-02-03 21:07:01 +0700

    add payment page

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+
 $(window).on("load", function () {
     $(".loader").fadeOut(900);
     $(".all").fadeIn(900);
 })
+
+
+let dataUser;
+
+const url = "http://foodmarket.test:8080/api/login/"
+
+
+var email = document.querySelector('#exampleInputEmail1').value
+var pasw = document.querySelector('#exampleInputPassword1').value
+
+// function login() {
+
+
+    // }
+
+
+    axios.post(url, {
+        email: 'rizki@gmail.com',
+        password: '12345678'
+
+    }).then((data) => {
+        const token = data.data
+        // dataUser = token
+        // document.querySelector('#exampleInputEmail1').value = token
+        // Token(token)
+        console.log(token)
+    }).catch((err) => {
+        console.log(err)
+    })
+
+// console.log(dataUser)
+
